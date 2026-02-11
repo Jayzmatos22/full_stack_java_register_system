68802165742440d6a98bb68b21a487059fae9e07
Jailton Santos <jailtonmatos200@gmail.com>
2026-02-11 14:56:34 -0300

    feat: Student entity with JPA and Lombok

---
+++ demo/src/main/java/Teste_sistema_cadastro/demo/model/Aluno.java
@@ -0,0 +1,25 @@
+package Teste_sistema_cadastro.demo.model;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Aluno {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(unique = true)
+    private String email;
+
+    private String senha;
+
+    private String faculdade;
+}
