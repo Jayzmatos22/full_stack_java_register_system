8837bef2f122809d5eda2c9db5aaba782b6338b2
Jailton Santos <jailtonmatos200@gmail.com>
2026-02-11 15:03:32 -0300

    feat: main application class and git settings

---
+++ demo/src/main/java/Teste_sistema_cadastro/demo/CadastroFsApplication.java
@@ -0,0 +1,13 @@
+package Teste_sistema_cadastro.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroFsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroFsApplication.class, args);
+	}
+
+}
